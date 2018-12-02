3b89d2a36da50ba2dc84e7b95fe5a50535ac63d1
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2018-12-02 16:55:08 -0700

    Decreaed indent on line 7

--- drills/version-1/index.js
+++ drills/version-1/index.js
@@ -2,19 +2,19 @@ var register = {
     transactions: []
  }
  
- function addTransaction(transaction){
-    register.transactions.push(transaction)
-    }
+function addTransaction(transaction){
+   register.transactions.push(transaction)
+}
  
- function getTotal(){
-    var total = 0
-    for(let i = 0; i < register.transactions.length; i++ ){
-        for(let j = 0; j < register.transactions[i].items.length; j++) {
-            total += (register.transactions[i].items[j].price * register.transactions[i].items[j].quantity)
-        }
-    }
-    return total
- }
+function getTotal(){
+   var total = 0
+   for(let i = 0; i < register.transactions.length; i++ ){
+      for(let j = 0; j < register.transactions[i].items.length; j++) {
+         total += (register.transactions[i].items[j].price * register.transactions[i].items[j].quantity)
+      }
+   }
+   return total
+}
 
  function getFormattedTotal(){
      return Number.parseFloat(getTotal().toString()).toFixed(2)
